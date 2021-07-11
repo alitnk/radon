87b03f1575795e7f5db6c512a90c538c2af4cbac
alitnk <ali.thenightkiller@gmail.com>
2021-07-11 14:57:16 +0430

    Added radon() and carbon() helpers

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,20 @@
 if (!function_exists('jnow')) {
     function jnow()
     {
-        return new \Wama\Radon\Radon(...func_get_args());
+        return new \Wama\Radon\Radon(null, ...func_get_args());
+    }
+}
+
+if (!function_exists('radon')) {
+    function radon()
+    {
+        return (new \Wama\Radon\Radon())->parse(...func_get_args());
+    }
+}
+
+if (!function_exists('carbon')) {
+    function carbon()
+    {
+        return (new \Carbon\Carbon(...func_get_args()));
     }
 }
